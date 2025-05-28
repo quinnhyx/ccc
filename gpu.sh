da870767890ab9a1fa47f4159d49e2e06a96372c
quinnhyx <123648507+quinnhyx@users.noreply.github.com>
2025-05-28 11:51:06 -0400

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -3,23 +3,20 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=64  # Adjust based on the required parallelism
 #SBATCH --partition=gpuq
-#SBATCH --output=ccc_%j.out
-#SBATCH --gres=gpu:2
-#SBATCH --time=1:00:00
+#SBATCH --output=clm_%j.out
+#SBATCH --gres=gpu:1
 
 # Load necessary modules
 module load python
 module load psutil  # If your script monitors resources
-module load nvidia-smi
 module load CUDA/11.2
 
 # Run Clustermatch with IPython
-srun --exact -n 1 --ntasks-per-node=64 --time=01:00:00 --pty bash -c "
-ipython3 -c '
+ipython3 -c "
 import numpy as np
 import pandas as pd
 from ccc.coef import ccc
-x = np.random.normal(size=10000)
-y = np.random.normal(size=10000)
-ccc(x, y, n_jobs=1) '
+x = np.random.normal(size=600000)
+y = np.random.normal(size=600000)
+%prun ccc(x, y, n_jobs=1)
 "
