43f77a249132b7c3fab14f3f9407fbe3429035df
Quinn He <qhe_smith_edu@login2.unity.rc.umass.edu>
2025-06-17 15:52:01 +0000

    profiling performance

--- run_ccc.sh
+++ run_ccc.sh
@@ -1,10 +1,50 @@
 #!/bin/bash
-#SBATCH --job-name=clustermatch_parallel
+#SBATCH --partition=gpu                # Use the same partition where gypsum-gpu106 belongs
+#SBATCH --nodelist=gypsum-gpu[106,108-115,118-119,121,124]       # Force it to run on this node
 #SBATCH --nodes=1
-#SBATCH --ntasks=64
-#SBATCH --partition=gpuq
-#SBATCH --output=gpu_%j.out
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:1080_ti:4           # Request 8 GTX 1080 Ti
+#SBATCH --cpus-per-task=24
+#SBATCH --time=08:00:00
+#SBATCH --mem=370g
+#SBATCH --job-name=ccc_4gpu
+#SBATCH --output=ccc_4gpu_%j.out
+#SBATCH --error=ccc_4gpu_%j.err
+#SBATCH --exclusive
 
-# Run your Python script
-python test_gpu_ccc.py
+
+PYTHON_SCRIPT=test_gpu_ccc.py
+
+# LOGFILE="8gpu_ccc_scaling.log"
+
+
+
+export NODES=1
+
+export GPUS_USED=4
+
+
+# echo "NODES GPUS_USED SIZE FEATURES TIME(s)" > logs/$LOGFILE
+
+
+
+for FEATURES in {2..20..2}; do
+
+  for SIZE in 10000 100000 1000000 10000000; do
+
+    export SIZE
+
+    export FEATURES
+
+
+
+#    echo "Running CCC with SIZE=$SIZE, FEATURES=$FEATURES..."
+
+    OUTPUT=$(python $PYTHON_SCRIPT)
+
+
+
+    # echo "$OUTPUT" | tee -a logs/$LOGFILE
+
+  done
+
+done
--- test_gpu_ccc.py
+++ test_gpu_ccc.py
@@ -1,23 +1,35 @@
 import numpy as np
-import pandas as pd
-from ccc.coef import ccc
+
+import os
+
 import time
 
-data_sizes = [i for i in range (100000,1000001,100000)]  # 100 to 10,000,000
-feature_counts = list(range(2, 21, 2))  # 2, 4, 6, ..., 20
+from ccc.coef import ccc
 
-print("Running CCC with GPU support...\n")
 
-for n_features in feature_counts:
-    for data_size in data_sizes:
-        print(f"Testing with {n_features} features and {data_size} samples...")
-        x = np.random.normal(size=(n_features, data_size))
 
-        start = time.time()
-        result = ccc(x)
-        end = time.time()
-        elapsed_ms = (end - start) * 1000
+SIZE = int(os.environ.get("SIZE", 1000000))
 
-        print(f"CCC result: {result}")
-        print(f"Time taken: {elapsed_ms:.2f} ms\n")
+FEATURES = int(os.environ.get("FEATURES", 10))
+
+NODES = int(os.environ.get("NODES", 1))
+
+GPUS_USED = int(os.environ.get("GPUS_USED", 1))
+
+
+
+x = np.random.normal(size=(FEATURES, SIZE))
+
+
+
+start = time.time()
+
+ccc(x)
+
+end = time.time()
+
+
+
+print(f"{NODES} {GPUS_USED} {SIZE} {FEATURES} {end - start:.4f}")
+
 
